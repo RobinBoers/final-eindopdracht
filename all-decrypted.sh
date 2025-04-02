37ae0537e4a30d150c1f6f8909984733ed1257a0
Robin <robindev2019@outlook.com>
2025-04-02 22:21:04 +0200

    Add script for easily running with decryptor

---
+++ all-decrypted.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+run() {
+  bun "$1"
+  sed -i 's|const people = require("./people.json");|const people = require("./decrypt.js");|' "$1"
+  bun "$1"
+  sed -i 's|const people = require("./decrypt.js");|const people = require("./people.json");|' "$1"
+}
+
+dir=$(dirname "$(realpath "$0")")
+
+for file in $(find "$dir" -maxdepth 1 -type f -name "*.js" | sort); do
+    run "$file"
+    echo
+    echo
+done
+
--- all.sh
+++ all.sh
@@ -3,7 +3,7 @@
 dir=$(dirname "$(realpath "$0")")
 
 for file in $(find "$dir" -maxdepth 1 -type f -name "*.js" | sort); do
-    node "$file"
+    bun "$file"
     echo
     echo
 done
